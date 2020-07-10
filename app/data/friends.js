6788832d30d8317e129d64e447ba347429f52d54
noisette <kasusky.music@gmail.com>
2020-07-10 01:39:07 -0700

    updated friends,js data

--- app/data/friends.js
+++ app/data/friends.js
@@ -21,36 +21,36 @@ var friends = [
       ]
     },
     {
-      name: "Geraldine",
+      name: "Geraldine Julienne",
 
-      photo: "",
+      photo: "https://vignette.wikia.nocookie.net/snicket/images/2/26/Geraldinebooksquare.jpg/revision/latest?cb=20191225233847",
       scores: [
       ]
     },
     {
-      name: "Jeremy Squalor",
+      name: "Jerome Squalor",
 
-      photo: "",
+      photo: "https://vignette.wikia.nocookie.net/snicket/images/f/f0/JeromeTV.png/revision/latest?cb=20180407004857",
       scores: [
       ]
     },
     {
-      name: "Josephine",
-      photo: "",
+      name: "Josephine Anwhistle",
+      photo: "https://vignette.wikia.nocookie.net/vfd/images/c/c5/JosephineTV.png/revision/latest?cb=20170117070638",
       scores: [
       ]
     },
     {
       name: "Dewey Denouement",
 
-      photo: "",
+      photo: "https://vignette.wikia.nocookie.net/snicket/images/3/33/EitherFrankOrErnest.png/revision/latest/top-crop/width/360/height/450?cb=20181204155545",
       scores: [
       ]
     },
     {
       name: "Beatrice Baudelaire",
 
-      photo: "",
+      photo: "https://vignette.wikia.nocookie.net/snicket/images/9/9c/Beatrice_netflix_square.png/revision/latest?cb=20190812203311",
       scores: [
       ]
     }
