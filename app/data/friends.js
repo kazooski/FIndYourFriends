91a652665f697a405357b973644885ccef3a16af
noisette <kasusky.music@gmail.com>
2020-07-10 02:36:58 -0700

    home.html from VIEW PAGE SOURCE

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,56 +3,67 @@
 
 
 var friends = [
-    {
-      name: "Ahmed",
-      
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-      scores: [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-    },
-    {
-      name: "Geraldine Julienne",
+  {
+    name: "Ahmed",
+    
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    name: "Geraldine Julienne",
 
-      photo: "https://vignette.wikia.nocookie.net/snicket/images/2/26/Geraldinebooksquare.jpg/revision/latest?cb=20191225233847",
-      scores: [
-      ]
-    },
-    {
-      name: "Jerome Squalor",
+    photo: "https://vignette.wikia.nocookie.net/snicket/images/2/26/Geraldinebooksquare.jpg/revision/latest?cb=20191225233847",
+    scores: [
+    ]
+  },
+  {
+    name: "Jerome Squalor",
 
-      photo: "https://vignette.wikia.nocookie.net/snicket/images/f/f0/JeromeTV.png/revision/latest?cb=20180407004857",
-      scores: [
-      ]
-    },
-    {
-      name: "Josephine Anwhistle",
-      photo: "https://vignette.wikia.nocookie.net/vfd/images/c/c5/JosephineTV.png/revision/latest?cb=20170117070638",
-      scores: [
-      ]
-    },
-    {
-      name: "Dewey Denouement",
+    photo: "https://vignette.wikia.nocookie.net/snicket/images/f/f0/JeromeTV.png/revision/latest?cb=20180407004857",
+    scores: [
+    ]
+  },
+  {
+    name: "Josephine Anwhistle",
+    photo: "https://vignette.wikia.nocookie.net/vfd/images/c/c5/JosephineTV.png/revision/latest?cb=20170117070638",
+    scores: [
+    ]
+  },
+  {
+    name: "Dewey Denouement",
 
-      photo: "https://vignette.wikia.nocookie.net/snicket/images/3/33/EitherFrankOrErnest.png/revision/latest/top-crop/width/360/height/450?cb=20181204155545",
-      scores: [
-      ]
-    },
-    {
-      name: "Beatrice Baudelaire",
+    photo: "https://vignette.wikia.nocookie.net/snicket/images/3/33/EitherFrankOrErnest.png/revision/latest/top-crop/width/360/height/450?cb=20181204155545",
+    scores: [
+    ]
+  },
+  {
+    name: "Beatrice Baudelaire",
 
-      photo: "https://vignette.wikia.nocookie.net/snicket/images/9/9c/Beatrice_netflix_square.png/revision/latest?cb=20190812203311",
-      scores: [
-      ]
-    }
-  
-  ];
+    photo: "https://vignette.wikia.nocookie.net/snicket/images/9/9c/Beatrice_netflix_square.png/revision/latest?cb=20190812203311",
+    scores: [
+    ]
+  },
+  {
+    name: "Montgomery Montgomery",
+
+    photo: "https://vignette.wikia.nocookie.net/snicket/images/6/64/Monty_netflix_square.png/revision/latest?cb=20190817232605",
+    scores: [
+    ]
+  }
+
+];
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
