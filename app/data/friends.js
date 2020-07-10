091d19a2d74dcbb491dadb6814a2aa46edd9341f
noisette <kasusky.music@gmail.com>
2020-07-10 01:28:50 -0700

    adding friends data

---
+++ app/data/friends.js
@@ -0,0 +1,58 @@
+// Friends info (DATA)
+// ===============================================================================
+
+
+var friends = [
+    {
+      name: "Ahmed",
+      
+      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      scores: [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+    },
+    {
+      name: "Geraldine",
+
+      photo: "",
+      scores: [
+      ]
+    },
+    {
+      name: "Jeremy Squalor",
+
+      photo: "",
+      scores: [
+      ]
+    },
+    {
+      name: "Josephine",
+      photo: "",
+      scores: [
+      ]
+    },
+    {
+      name: "Dewey Denouement",
+
+      photo: "",
+      scores: [
+      ]
+    },
+    {
+      name: "Beatrice Baudelaire",
+
+      photo: "",
+      scores: [
+      ]
+    }
+  
+  ];
